b89db300636228c418c7d5b6cc0a5dc95157370c
Axel Mora <axelmora2011@gmail.com>
2024-10-21 13:33:39 -0700

    Updated my About Me section

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,32 +5,13 @@ import AxelMoraLogBar from "../assets/AxelMoraLogBar.png";
 import AxelMoraCurlBar from "../assets/AxelMoraCurlBar.png";
 import LogBar from "../assets/LogBar.png";
 
-export const HERO_CONTENT = `Powerlifter, Strongman, and tech enthusiast based in Oxnard, CA. Lover of BBQ, music, soccer, and all things fitness.`; 
+export const HERO_CONTENT = `Powerlifter, Strongman, fitness & tech enthusiast.`; 
 
-export const ABOUT_TEXT = `I grew up in Oxnard, CA, with a passion for sports, including soccer, basketball, volleyball, cross country, and track & field. 
-I'm an avid soccer fan and love following the sport. My favorite soccer team is Club America from Liga MX.
-
-During my time at Oxnard High School, I competed in volleyball for one year and spent three years running cross country and track, with my main events in track being the 800m and 1600m.
-I also played soccer in the local recreation leagues. 
-After high school, I continued my athletic journey at Ventura College, running cross country and track, where our team made it to the California state finals in 2017. 
-Since 2021, I’ve shifted my focus to powerlifting and, more recently, strongman training at Dogg Yard Gym in Ventura, CA. 
-I started humbly, using the 35 lb barbell for the core powerlifting movements—bench press, squats, and deadlifts. 
-Since then, I’ve made significant progress and plan to compete in strongman competitions starting in 2025. 
-
-Aside from my athletic pursuits, I earned my B.S. in Computer Science from CSU Channel Islands in December 2023.
-
-I'm passionate about working on mobile apps, cloud computing, networking & infrastructure, and currently gaining experience with web apps & websites.
-
-When I’m not training or coding, I enjoy all kinds of music—rap, rock, metal, pop, you name it. In the gym, I love listening to west coast rap and metal bands like Metallica, Iron Maiden, and Megadeth to get pumped up.
-
-I’m a huge foodie. My favorite cuisine is Mexican, and I can never resist enchiladas de mole. I also love seafood, Italian, sushi, French, and BBQ.
-
-Speaking of BBQ, I love smoking ribs in my backyard, and my favorite dish to make is St. Louis-style ribs. And when it comes to desserts, ice cream is my ultimate weakness.
-
-My favorite movie genres are action, sci-fi, and horror. I dream of traveling to soccer stadiums in Europe and Latin America. 
-
-I’m inspired by athletes like Brian Shaw and Hafthor Bjornsson, and when I'm not working out, you'll probably find me catching up on my favorite tv shows and films or experimenting with new BBQ recipes.`;
-
+export const ABOUT_TEXT = `I’m a fitness enthusiast with a focus on powerlifting and strongman.
+                          Since 2021, I’ve dedicated myself to building strength and plan to compete in strongman competitions in the future.
+                          When I’m not lifting, I enjoy exploring new music, especially west coast rap and metal to fuel my workouts.
+                          I’m also passionate about BBQ, St. Louis-style ribs are my specialty, and I love experimenting with new recipes.
+                          Inspired by strength & strongman athletes, I’m always pushing my limits both in and out of the gym.`;
 
 export const CONTACT = {
   email: "axelmora2011@gmail.com",
